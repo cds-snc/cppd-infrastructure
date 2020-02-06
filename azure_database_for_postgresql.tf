78b37c0369a295f309ca14fb35db93459c0e57f1
Kyle Meelker <kyle.meelker@gmail.com>
2020-02-06 09:14:09 -0500

    removed the location line from the key vault declaration

--- azure_database_for_postgresql.tf
+++ azure_database_for_postgresql.tf
@@ -1,6 +1,5 @@
 data "azurerm_key_vault" "central-key-vault" {
   name                = "central-key-vault"
-  # location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
 }
 
